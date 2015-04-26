1f76fb506c6b8de262926dd0e0073628fd9ae9ee
presian <presiandanailov@gmail.com>
2015-04-26 11:55:34 +0300

    Construct models instance in the master controller

--- controllers/master.php
+++ controllers/master.php
@@ -5,11 +5,24 @@ namespace Controllers;
 class Master_Controller{
     
     protected $layout;
-
     protected $viewsDir;
     
-    public function __construct($viewsDir = '/views/master') {
+    public function __construct(
+            $className = '\Controllers\Master_Controller',
+            $model = 'master',
+            $viewsDir = '/views/master') {
+        
         $this->viewsDir = $viewsDir;
+        $this->className = $className;
+        
+        include_once ROOT_DIR . "/models/{$model}.php";
+        $modelClass = "\Models\\" . ucfirst($model) . '_Model';
+        $this->model = new $modelClass(
+            array(
+                'table'=> 'none'
+            )
+        );
+        
         $this->layout = ROOT_DIR . '/views/layouts/default.php';
     }
 }
